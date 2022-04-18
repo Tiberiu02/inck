59f3b1c3873de9f239fd426c73986243923db87b
Tiberiu Musat <musattiberiu@gmail.com>
2022-04-18 13:28:01 +0200

    Note id in html title

--- client/pages/note/[id].js
+++ client/pages/note/[id].js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Script from 'next/script'
 import { FullScreenButton } from '/components/fsButton'
+import { useRouter } from 'next/router'
 
 export default function Canvas() {
+  const router = useRouter()
+  const { id } = router.query
+
   return (
     <div className='touch-none'>
       <Head>
-        <title>Inck</title>
+        <title>Inck/{id}</title>
         <meta name="description" content="The only ink that you will ever need" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
