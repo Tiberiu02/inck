fcc1500b19e78bc5b12e08ed4bfe58630fd58785
Tiberiu Musat <musattiberiu@gmail.com>
2022-04-22 02:43:39 +0200

    design tweaks

--- client/pages/explorer.js
+++ client/pages/explorer.js
@@ -14,7 +14,7 @@ function DirListing({ Symbol, symbolClassName, name, className, children, link }
   if (!Symbol)
     Symbol = open ? Icon.FcOpenedFolder : Icon.FcFolder
   if (link)
-    open = selected
+    open = false
   return (
     <div className='min-w-full w-fit'>
       <button className={'flex flex-row items-center gap-2 px-4 py-2 outline-none min-w-full w-fit ' + (selected ? 'bg-gray-200 ' : '') + className}
@@ -43,12 +43,12 @@ export default function Explorer() {
         <div className='flex flex-col w-[100vw] h-[100vh] font-round'>
 
           { /** Navigation bar */}
-          <div className='flex flex-row justify-between h-16 items-center px-8 border-b-[1px] border-gray-300'>
-            <div className='flex flex-row gap-3 items-center text-primary'>
-              <Icon.FaPencilAlt className='text-2xl mt'/>
-              <p className='font-cursive font-bold tracking-wider text-2xl'>Inck</p>
+          <div className='flex flex-row justify-between h-16 items-center px-6 border-b-[1px] bg-gray-100 border-gray-300'>
+            <div className='flex flex-row gap-3 items-center text-gray-800'>
+              <Icon.FaPencilAlt className='text-2xl'/>
+              <p className='font-extrabold tracking-wider text-2xl text-gr mt-[0.1rem]'>Inck</p>
             </div>
-            <div className='bg-gray-100 text-gray-500 flex flex-row items-center h-10 w-full max-w-xl rounded-lg'>
+            <div className='bg-white border-[1px] border-gray-300 text-gray-500 flex flex-row items-center h-10 w-full max-w-xl rounded-lg'>
               <button className='group pl-2 h-10 flex items-center justify-center'>
                 <div className='group-hover:bg-gray-300 flex items-center justify-center w-8 h-8 rounded-full'>
                   <Icon.FaSearch />
@@ -71,53 +71,56 @@ export default function Explorer() {
 
           <div className='h-full w-full flex flex-row pt-4 overflow-hidden'>
             { /** Directory browser */}
-            <div className='h-full w-96 border-r-[1px] flex flex-col' style={{overflow: 'overlay'}}>
-              <DirListing Symbol={Icon.FcClock} symbolClassName='mt-[0.1rem]' name='Recent' link></DirListing>
-              <DirListing Symbol={Icon.FcGlobe} symbolClassName='mt-[0.1rem]' name='Shared with me' link></DirListing>
-              <DirListing Symbol={Icon.FcBookmark} name='Homework' link></DirListing>
-              <DirListing Symbol={Icon.FcFullTrash} name='Trash' link></DirListing>
+            <div className='h-full w-96 border-r-[1px] border-gray-300 flex flex-col' style={{overflow: 'overlay'}}>
+              <div className='min-w-full w-fit'>
 
-              <DirListing Symbol={Icon.FcNook} className='mt-10' name='My Notes'>
-                <DirListing name='Epfl' className='pl-12'>
-                  <DirListing name='Analyse' className='pl-20'>
-                    <DirListing name='w1' className='pl-28'></DirListing>
-                    <DirListing name='w2' className='pl-28'></DirListing>
-                    <DirListing name='w3' className='pl-28'></DirListing>
-                    <DirListing name='w4' className='pl-28'></DirListing>
-                    <DirListing name='w5' className='pl-28'></DirListing>
-                    <DirListing name='w6' className='pl-28'></DirListing>
-                    <DirListing name='w7' className='pl-28'></DirListing>
-                  </DirListing>
-                  <DirListing name='AICC 2' className='pl-20'>
-                    <DirListing name='w1' className='pl-28'></DirListing>
-                    <DirListing name='w2' className='pl-28'></DirListing>
-                    <DirListing name='w3' className='pl-28'></DirListing>
-                    <DirListing name='w4' className='pl-28'></DirListing>
-                    <DirListing name='w5' className='pl-28'></DirListing>
-                    <DirListing name='w6' className='pl-28'></DirListing>
-                    <DirListing name='w7' className='pl-28'></DirListing>
-                  </DirListing>
-                  <DirListing name='Pratique de la programation orientée objet' className='pl-20'>
-                    <DirListing name='w1' className='pl-28'></DirListing>
-                    <DirListing name='w2' className='pl-28'></DirListing>
-                    <DirListing name='w3' className='pl-28'></DirListing>
-                    <DirListing name='w4' className='pl-28'></DirListing>
-                    <DirListing name='w5' className='pl-28'></DirListing>
-                    <DirListing name='w6' className='pl-28'></DirListing>
-                    <DirListing name='w7' className='pl-28'></DirListing>
-                  </DirListing>
-                  <DirListing name='Digital system design' className='pl-20'>
-                    <DirListing name='w1' className='pl-28'></DirListing>
-                    <DirListing name='w2' className='pl-28'></DirListing>
-                    <DirListing name='w3' className='pl-28'></DirListing>
-                    <DirListing name='w4' className='pl-28'></DirListing>
-                    <DirListing name='w5' className='pl-28'></DirListing>
-                    <DirListing name='w6' className='pl-28'></DirListing>
-                    <DirListing name='w7' className='pl-28'></DirListing>
+                <DirListing Symbol={Icon.FcClock} symbolClassName='mt-[0.1rem]' name='Recent' link></DirListing>
+                <DirListing Symbol={Icon.FcGlobe} symbolClassName='mt-[0.1rem]' name='Shared with me' link></DirListing>
+                <DirListing Symbol={Icon.FcBookmark} name='Homework' link></DirListing>
+                <DirListing Symbol={Icon.FcFullTrash} name='Trash' link></DirListing>
+
+                <DirListing Symbol={Icon.FcNook} className='mt-10' name='My Notes'>
+                  <DirListing name='Epfl' className='pl-12'>
+                    <DirListing name='Analyse' className='pl-20'>
+                      <DirListing name='w1' className='pl-28'></DirListing>
+                      <DirListing name='w2' className='pl-28'></DirListing>
+                      <DirListing name='w3' className='pl-28'></DirListing>
+                      <DirListing name='w4' className='pl-28'></DirListing>
+                      <DirListing name='w5' className='pl-28'></DirListing>
+                      <DirListing name='w6' className='pl-28'></DirListing>
+                      <DirListing name='w7' className='pl-28'></DirListing>
+                    </DirListing>
+                    <DirListing name='AICC 2' className='pl-20'>
+                      <DirListing name='w1' className='pl-28'></DirListing>
+                      <DirListing name='w2' className='pl-28'></DirListing>
+                      <DirListing name='w3' className='pl-28'></DirListing>
+                      <DirListing name='w4' className='pl-28'></DirListing>
+                      <DirListing name='w5' className='pl-28'></DirListing>
+                      <DirListing name='w6' className='pl-28'></DirListing>
+                      <DirListing name='w7' className='pl-28'></DirListing>
+                    </DirListing>
+                    <DirListing name='Pratique de la programation orientée objet' className='pl-20'>
+                      <DirListing name='w1' className='pl-28'></DirListing>
+                      <DirListing name='w2' className='pl-28'></DirListing>
+                      <DirListing name='w3' className='pl-28'></DirListing>
+                      <DirListing name='w4' className='pl-28'></DirListing>
+                      <DirListing name='w5' className='pl-28'></DirListing>
+                      <DirListing name='w6' className='pl-28'></DirListing>
+                      <DirListing name='w7' className='pl-28'></DirListing>
+                    </DirListing>
+                    <DirListing name='Digital system design' className='pl-20'>
+                      <DirListing name='w1' className='pl-28'></DirListing>
+                      <DirListing name='w2' className='pl-28'></DirListing>
+                      <DirListing name='w3' className='pl-28'></DirListing>
+                      <DirListing name='w4' className='pl-28'></DirListing>
+                      <DirListing name='w5' className='pl-28'></DirListing>
+                      <DirListing name='w6' className='pl-28'></DirListing>
+                      <DirListing name='w7' className='pl-28'></DirListing>
+                    </DirListing>
                   </DirListing>
                 </DirListing>
-              </DirListing>
               
+              </div>
             </div>
           </div>
 
