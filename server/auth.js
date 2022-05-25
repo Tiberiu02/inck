68fb97e75578d9106b837a942ee533e42be89f8d
Tiberiu Musat <musattiberiu@gmail.com>
2022-05-25 10:30:49 +0200

    auth polish

--- server/auth.js
+++ server/auth.js
@@ -3,13 +3,6 @@ const { UserModel } = require("./models");
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
-function generateToken(obj) {
-  return jwt.sign(
-    obj,
-    process.env.JWT_TOKEN,
-  );
-}
-
 /**
  * Potential errors w/ status:
  * 400: missing fields, invalid email
@@ -23,45 +16,40 @@ async function register(req, res){
     const cookies = req.cookies
     console.log(cookies['authToken'])
 
-    const{ firstName, lastName, email, password } = req.body;
-    if (!(email && password && firstName && lastName)) {
-      return res.status(400).send({error: "missing fields"});
-    }
+    let { firstName, lastName, email, password } = req.body;
+    email = email.trim().toLowerCase();
+
+    if (!(email && password && firstName && lastName))
+      return res.status(400).send({error: "missing fields"})
     
-    if(!validateEmail(email)) {
+    if(!validateEmail(email))
         return res.status(400).send({error: "invalid email"})
-    }
 
-    const oldUser = await UserModel.findOne({ email });
-
-    if (oldUser) {
+    if (await UserModel.findOne({ email })) 
       return res.status(409).send({error: "user already exists"});
-    }
 
     encryptedPassword = await bcrypt.hash(password, 10);
 
     const user = await UserModel.create({
       firstName,
       lastName,
-      email: email.trim().toLowerCase(),
+      email,
       registrationDate: new Date().toLocaleDateString("en-us", {timeZone: 'UTC'}),
       // TODO for prod: Set to false, request verification by email + phone
       activeAccount: true,
-      password: encryptedPassword,
     });
 
     // Auth token
     const token = jwt.sign(
       { 
         userId: user._id, 
-        email: user.email , 
-        password: user.password,
+        email: user.email,
       },
       process.env.JWT_TOKEN,
     );
     
     user.token = token;
-    res.status(201).send({email: user.email, token: token});
+    res.status(201).send({ email, token });
 
   } catch (err) {
     console.log(err);
@@ -71,32 +59,33 @@ async function register(req, res){
 
 async function login(req, res){
   try {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
+    email = email.trim().toLowerCase();
 
-    if (!(email && password)) {
+    if (!(email && password))
       res.status(400).send({error: "missing fields"});
-    }
 
-    if (!validateEmail(email)) {
+    if (!validateEmail(email))
       return res.status(400).send({error: "invalid email"})
-    }
 
     const user = await UserModel.findOne({ email });
     if (user && (await bcrypt.compare(password, user.password))) {
-        const token = generateToken(
+        const token = jwt.sign(
           { 
             userId: user._id, 
-            email: user.email, 
-            password: user.password,
+            email: user.email,
           },
           process.env.JWT_TOKEN,
         )
 
-      return res.status(200).send({ email: user.email, token: token })
+      return res.status(200).send({ email, token })
     }
+
     return res.status(400).send({error: "invalid credentials"});
+
   } catch (err) {
     console.log(err);
+    res.status(400).send({error: "internal error"})
   }
 }
 
