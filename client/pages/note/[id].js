9cfd7b32eb315173c36f9b9bbfd681772c80b403
Tiberiu Musat <musattiberiu@gmail.com>
2022-07-23 23:05:42 +0300

    Fixed note page title

--- client/pages/note/[id].js
+++ client/pages/note/[id].js
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import Script from 'next/script'
-import { FullScreenButton } from '../../components/FullScreenButton'
-import { useRouter } from 'next/router'
-import Canvas from '../../components/Canvas'
+import Head from "next/head";
+import Script from "next/script";
+import { FullScreenButton } from "../../components/FullScreenButton";
+import { useRouter } from "next/router";
+import Canvas from "../../components/Canvas";
 
 export default function Note() {
-  const router = useRouter()
-  const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
   return (
     <div>
       <Head>
-        <title>Inck/{id}</title>
+        <title>{id ? `Inck/${id}` : "Inck"}</title>
         <meta name="description" content="The only ink that you will ever need" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Canvas />
       <FullScreenButton />
     </div>
-  )
+  );
 }
