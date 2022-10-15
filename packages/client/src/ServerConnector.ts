8b597773ce263852865bc933a2cec336cd553dc5
Tiberiu Musat <musattiberiu@gmail.com>
2022-10-15 05:23:18 +0200

    Removed generated API skeleton and used JS Proxy for client stub instead

--- packages/client/src/ServerConnector.ts
+++ packages/client/src/ServerConnector.ts
@@ -1,42 +1,46 @@
-import type { API } from "@inck/server/src/api/index";
-import { ServerApiSkeleton } from "../server-api-skeleton";
+import type { ApiInterface } from "@inck/server/src/api/index";
 
-function GetApiPath(path) {
-  if (window) {
-    return `${window.location.protocol}//${window.location.hostname}:8080${path}`;
-  }
-}
+async function ApiFetch(path: string, args: any[]) {
+  const url = `${window.location.protocol}//${window.location.hostname}:8080${path}`;
 
-function BuildApiConnector(api: any, path: string) {
-  if (typeof api == "object") {
-    const connector = {};
-    for (const endpoint in api) {
-      connector[endpoint] = BuildApiConnector(api[endpoint], `${path}/${endpoint}`);
-    }
-    return connector;
+  const response = await fetch(url, {
+    method: "post",
+    body: JSON.stringify(args),
+    headers: {
+      "Content-type": "application/json;charset=UTF-8",
+    },
+  });
+
+  const json = await response.json();
+
+  if (response.ok) {
+    return json.result;
+  } else if (json.error) {
+    throw new Error(json.error);
   } else {
-    return async (...args: any[]) => {
-      console.log("server request", path, args);
-
-      const response = await fetch(GetApiPath(path), {
-        method: "post",
-        body: JSON.stringify(args),
-        headers: {
-          "Content-type": "application/json;charset=UTF-8",
-        },
-      });
-
-      const json = await response.json();
-
-      if (response.ok) {
-        return json.result;
-      } else if (json.error) {
-        throw new Error(json.error);
-      } else {
-        throw new Error("Connection to the server api failed, invalid response.");
-      }
-    };
+    throw new Error("Connection to the server api failed, invalid response.");
   }
 }
 
-export const HttpServer = BuildApiConnector(ServerApiSkeleton, "/api") as typeof API;
+function BuildApiConnector(path: string) {
+  return new Proxy((...args: any[]) => ApiFetch(path, args), {
+    get(_obj, endpoint) {
+      if (typeof endpoint !== "string" || endpoint === "then") {
+        // special case for if the proxy is accidentally treated
+        // like a PromiseLike (like in `Promise.resolve(proxy)`)
+        return undefined;
+      }
+      const x = BuildApiConnector(`${path}/${endpoint}`);
+      return x;
+    },
+  });
+}
+
+type PromiseWrapper<T extends any> = T extends Promise<infer I> ? T : Promise<T>;
+type AsyncWrapper<T> = T extends (...args: any) => any
+  ? (...args: Parameters<T>) => PromiseWrapper<ReturnType<T>>
+  : {
+      [key in keyof T]: AsyncWrapper<T[key]>;
+    };
+
+export const HttpServer = BuildApiConnector("/api") as AsyncWrapper<ApiInterface>;
