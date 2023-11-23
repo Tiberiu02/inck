9cd7cc1db771ebf6b5d46ab8f47ea9a2fd6460ae
Tiberiu <musattiberiu@gmail.com>
2023-11-23 14:37:06 +0100

    Fix LoadingNoteAnimation

--- packages/client/src/pages/note/[id].tsx
+++ packages/client/src/pages/note/[id].tsx
@@ -19,7 +19,7 @@ export default function Note() {
 
       <Canvas />
       <FullScreenButton />
-      <LoadingNoteAnimation />
+      {/* <LoadingNoteAnimation /> */}
       <OfflineWarning />
     </div>
   );
